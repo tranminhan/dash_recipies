3bf5425f45185db737a6b3e9410cb0075b16e44d
An Tran <an@tinypulse.com>
2020-10-05 09:49:44 +0700

    add reference

---
+++ README.md
@@ -0,0 +1,7 @@
+References
+- [ ] https://pbpython.com/plotly-dash-intro.html
+- [ ] https://morioh.com/p/63c334dee27b
+- [ ] https://www.statworx.com/ch/blog/how-to-build-a-dashboard-in-python-plotly-dash-step-by-step-tutorial/
+- [ ] https://dash.plotly.com/external-resources
+- [ ] https://dash.plotly.com/urls
+- [ ] https://dash.plotly.com/deployment
--- tutorials/ch04-interactive/simple_app.py
+++ tutorials/ch04-interactive/simple_app.py
@@ -3,11 +3,13 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+
+import dash_bootstrap_components as dbc
+
 import plotly.express as px
 import pandas as pd
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 styles = {
   'pre': {
